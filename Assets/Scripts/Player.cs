68042e2fb800f9f821f16b083011638f032a65ff
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-12-14 20:17:55 -0300

    feat: add MoveControllers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Player : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject enemy;
-    // Start is called before the first frame update
+    [SerializeField]
+    public Transform pos ;
+    public float dirX = 0.0f;
+    public float dirY = 0.0f;
+    public float speed = 0.0f;
+    public CharacterController characterController;
+
     void Start()
     {
-
+        pos = this.GetComponent<Transform>();
+        characterController = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ActionMove();
     }
+
+    private void ActionMove()
+    {
+        dirX = Input.GetAxis("Horizontal") * speed;
+        dirY = Input.GetAxis("Vertical") * speed;
+        
+        Vector3 move= new Vector3(dirX, dirY, 0) * Time.deltaTime;
+
+        characterController.Move(move);
+    }
+
 }
