8a26471827ed492504bccc8f1c97e0e2777611af
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-12-14 23:47:04 -0300

    feat: add PlayerController

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,36 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
+using PlayerManager;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    public Transform pos ;
-    public float dirX = 0.0f;
-    public float dirY = 0.0f;
-    public float speed = 0.0f;
-    public CharacterController characterController;
+    private PlayerController playerController;
 
     void Start()
     {
-        pos = this.GetComponent<Transform>();
-        characterController = this.GetComponent<CharacterController>();
+        playerController = new PlayerController(this.GetComponent<CharacterController>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        ActionMove();
-    }
-
-    private void ActionMove()
-    {
-        dirX = Input.GetAxis("Horizontal") * speed;
-        dirY = Input.GetAxis("Vertical") * speed;
-        
-        Vector3 move= new Vector3(dirX, dirY, 0) * Time.deltaTime;
-
-        characterController.Move(move);
+        playerController.ActionMove();
     }
 
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerManager
+{
+
+    public class PlayerController
+    {
+        public CharacterController characterController;
+        public float speed { get; set; } = 10.0f;
+
+        public PlayerController(CharacterController character)
+        {
+            characterController = character;
+        }
+
+        public void ActionMove()
+        {
+            float dirX = Input.GetAxis("Horizontal") * speed;
+            float dirY = Input.GetAxis("Vertical") * speed;
+
+            Vector3 move = new Vector3(dirX, dirY, 0) * Time.deltaTime;
+
+            characterController.Move(move);
+        }
+    }
+
+}
